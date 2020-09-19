dac6ec26b475ed8ee68a626db9ef0b5a47e6ff45
Mar Alguacil <maralguacil@correo.ugr.es>
2020-09-19 13:09:00 +0200

    ResNet block that uses SPADE #2

--- model/spade.py
+++ model/spade.py
@@ -1,20 +1,54 @@
 """
-    SPatially-Adaptive (DE)normalization (SPADE)
+    SPADE Architecture.
 
     Author: Mar Alguacil
 """
 import tensorflow as tf
 
 
+def spade_resblk(segmap, x, k_out):
+    """
+        Residual block.
+
+        Takes in the segmentation map (|segmap|) as input, learns the skip connection if necessary, and applies normalization
+        first and then convolution.
+    """
+    k_in = tf.shape(x)[-1]
+    k_middle = min(k_in, k_out)
+
+    with tf.compat.v1.variable_scope('SPADE ResBlk'):
+        dx = spade(segmap, x, k_in)
+        dx = leaky_relu(dx)
+        dx = tf.keras.layers.Conv2D(k_middle, kernel_size=3, padding='same')(dx)
+
+        dx = spade(segmap, dx, k_middle)
+        dx = leaky_relu(dx)
+        dx = tf.keras.layers.Conv2D(k_out, kernel_size=3, padding='same')(dx)
+
+        if (k_in!=k_out):
+            x_s = spade(segmap, x, k_in)
+            # x_s = leaky_relu(x_s)
+            x_s = tf.keras.layers.Conv2D(k_out, kernel_size=1, padding='same', use_bias=False)(x_s)
+        else:
+            x_s = x
+
+        return x_s + dx
+
+def leaky_relu(x):
+    """
+        Compute the Leaky ReLU activation function.
+    """
+    return tf.nn.leaky_relu(x, 0.2)
+
 def spade(segmap, x, k_filters, nhidden=128, kernel_size=(3,3)):
     """
-        Spatially-Adaptive Normalization.
+        SPatially-Adaptive (DE)normalization (SPADE)
 
           In the SPADE, the segmentation map (|segmap|) is first projected onto an embedding space and then convolved to
         produce the modulation parameters |alpha| and |beta|. The produced |alpha| and |beta| are multiplied and added to
         the normalized activation element-wise (Park et al.).
     """
-    with tf.compat.v1.variable_scope('spade'):
+    with tf.compat.v1.variable_scope('SPADE'):
         # Cast the semantic segmentation mask to float
         segmap = tf.cast(segmap, 'float')
 
