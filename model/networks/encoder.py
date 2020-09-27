a3e3cc227d0a7b0a989a664acf1f2c186c5cf23f
Mar Alguacil <maralguacil@correo.ugr.es>
2020-09-27 18:54:33 +0200

    Close #5 Minor change

--- model/networks/encoder.py
+++ model/networks/encoder.py
@@ -16,36 +16,37 @@ def encoder(x, crop_size, num_filters=64):
     # pw = 1 #int(np.ceil((kw - 1.0) / 2))
     last_ndf = num_filters*8
 
-    if tf.shape(x)[1] != 256 or tf.shape(x)[2] != 256:
-        tf.image.resize(images=x, size=[256, 256],
-                        method=tf.image.ResizeMethod.BILINEAR)
+    with tf.compat.v1.variable_scope('Image Encoder'):
+        if tf.shape(x)[1] != 256 or tf.shape(x)[2] != 256:
+            tf.image.resize(images=x, size=[256, 256],
+                            method=tf.image.ResizeMethod.BILINEAR)
 
-    x = Conv2d(x, num_filters, kernel_size=3, padding=1, strides=2)
-    x = instance_normalization(x)
-    x = leaky_relu(x)
-
-    x = Conv2d(x, num_filters*2, kernel_size=3, padding=1, strides=2)
-    x = instance_normalization(x)
-    x = leaky_relu(x)
-
-    x = Conv2d(x, num_filters*4, kernel_size=3, padding=1, strides=2)
-    x = instance_normalization(x)
-    x = leaky_relu(x)
-
-    x = Conv2d(x, last_ndf, kernel_size=3, padding=1, strides=2)
-    x = instance_normalization(x)
-    x = leaky_relu(x)
-
-    x = Conv2d(x, last_ndf, kernel_size=3, padding=1, strides=2)
-    x = instance_normalization(x)
-
-    if crop_size >= 256:
+        x = Conv2d(x, num_filters, kernel_size=3, padding=1, strides=2)
+        x = instance_normalization(x)
         x = leaky_relu(x)
+
+        x = Conv2d(x, num_filters*2, kernel_size=3, padding=1, strides=2)
+        x = instance_normalization(x)
+        x = leaky_relu(x)
+
+        x = Conv2d(x, num_filters*4, kernel_size=3, padding=1, strides=2)
+        x = instance_normalization(x)
+        x = leaky_relu(x)
+
+        x = Conv2d(x, last_ndf, kernel_size=3, padding=1, strides=2)
+        x = instance_normalization(x)
+        x = leaky_relu(x)
+
         x = Conv2d(x, last_ndf, kernel_size=3, padding=1, strides=2)
         x = instance_normalization(x)
 
-    x = leaky_relu(x)
+        if crop_size >= 256:
+            x = leaky_relu(x)
+            x = Conv2d(x, last_ndf, kernel_size=3, padding=1, strides=2)
+            x = instance_normalization(x)
 
-    x = tf.reshape(x, [tf.shape(x)[0], -1])
-    return tf.keras.layers.Dense(256)(x), tf.keras.layers.Dense(256)(x) # mean, variance
+        x = leaky_relu(x)
+
+        x = tf.reshape(x, [tf.shape(x)[0], -1])
+        return tf.keras.layers.Dense(256)(x), tf.keras.layers.Dense(256)(x) # mean, variance
 
