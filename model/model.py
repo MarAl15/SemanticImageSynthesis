96c4d0c4265050eb7fa83956587e8e18c01f8933
Mar Alguacil <maralguacil@correo.ugr.es>
2020-10-07 16:45:06 +0200

    Remove useless options #8

--- model/model.py
+++ model/model.py
@@ -11,23 +11,50 @@ from model.networks.loss import *
 
 class Model(object):
     def __init__(self, sess, args):
-        self.model_name = 'Model'
+        # self.model_name = 'Model'
 
         self.sess = sess
-        self.opt = args
 
-    def create_optimizers(self, generator_loss, discriminator_loss):
+        # Loss weights
+        self.lambda_kld = args.lambda_kld
+        self.lambda_features = args.lambda_features
+        self.lambda_vgg = args.lambda_vgg
+
+        # Use or not image encoder
+        self.use_vae = args.use_vae
+
+        # Use or not discriminator feature matching loss
+        self.no_feature_loss = args.no_feature_loss
+
+        # Use or not VGG loss
+        self.no_vgg_loss = args.no_vgg_loss
+
+        # Discriminator options
+        self.num_discriminators = args.num_discriminators
+        self.num_discriminator_filters = args.num_discriminator_filters
+        self.num_discriminator_layers = args.num_discriminator_layers
+
+        # Encoder options
+        self.crop_size = args.crop_size
+        self.num_encoder_filters = args.num_encoder_filters
+
+        # Generator options
+        self.num_upsampling_layers = args.num_upsampling_layers
+        self.num_generator_filters = args.num_generator_filters
+        self.z_dim = args.z_dim
+
+    def create_optimizers(self, generator_loss, discriminator_loss, lr, beta1, beta2, no_TTUR=True):
         """Constructs the generator and discriminator optimizers."""
         train_vars = tf.compat.v1.trainable_variables()
         generator_vars = [var for var in train_vars if 'Encoder' in var.name or 'Generator' in var.name]
         discriminator_vars = [var for var in train_vars if 'Discriminator' in var.name]
 
-        (g_lr, d_lr) = (self.opt.lr/2, self.opt.lr*2) if not self.opt.no_TTUR else (self.opt.lr, self.opt.lr)
+        (g_lr, d_lr) = (lr/2, lr*2) if not no_TTUR else (lr, lr)
 
         # generator_optimizer, discriminator_optimizer
-        return tf.compat.v1.train.AdamOptimizer(g_lr, beta1=self.opt.beta1, beta2=self.opt.beta2).\
+        return tf.compat.v1.train.AdamOptimizer(g_lr, beta1=beta1, beta2=beta2).\
                   minimize(generator_loss, var_list=generator_vars), \
-               tf.compat.v1.train.AdamOptimizer(d_lr, beta1=self.opt.beta1, beta2=self.opt.beta2).\
+               tf.compat.v1.train.AdamOptimizer(d_lr, beta1=beta1, beta2=beta2).\
                   minimize(discriminator_loss, var_list=discriminator_vars),
 
 
@@ -44,30 +71,29 @@ class Model(object):
         """
         generator_losses, discriminator_losses = {}, {}
 
-        if self.opt.use_vae:
+        if self.use_vae:
             fake_image, generator_losses['KLD'] = self.generate_fake(segmap_image, real_image,
                                                                      compute_kld_loss=True)
         else:
             fake_image, _ = self.generate_fake(segmap_image, real_image)
 
         pred_real, pred_fake = self.discriminate(segmap_image, real_image, fake_image,
-                                                 get_intermediate_features=(not self.opt.no_feature_loss))
+                                                 get_intermediate_features=(not self.no_feature_loss))
 
         generator_losses['Hinge'] = hinge_loss_generator(pred_fake)
 
-        if not self.opt.no_feature_loss:
+        if not self.no_feature_loss:
             generator_losses['Feature'] = 0.0
-            # self.opt.num_discriminators = len(pred_fake)
-            for i in range(self.opt.num_discriminators):
+            # self.num_discriminators = len(pred_fake)
+            for i in range(self.num_discriminators):
                 # Last output is the final prediction, so it is excluded
                 for j in range(len(pred_fake[i])-1): # for each layer output
-                    # generator_losses['Feature'] += l1_loss(pred_fake[i][j], pred_real[i][j]) * self.opt.lambda_features/self.opt.num_discriminators
                     # generator_losses['Feature'] += l1_loss(pred_fake[i][j], tf.stop_gradient(pred_real[i][j]))
                     generator_losses['Feature'] += l1_loss(pred_fake[i][j], pred_real[i][j])
-            generator_losses['Feature'] *= self.opt.lambda_features/self.opt.num_discriminators
+            generator_losses['Feature'] *= self.lambda_features/self.num_discriminators
 
-        if not self.opt.no_vgg_loss:
-            generator_losses['VGG'] = vgg_loss(real_image, fake_image) * self.opt.lambda_vgg
+        if not self.no_vgg_loss:
+            generator_losses['VGG'] = vgg_loss(real_image, fake_image) * self.lambda_vgg
 
         discriminator_losses['HingeReal'], discriminator_losses['HingeFake'] = hinge_loss_discriminator(pred_real, pred_fake)
 
@@ -77,9 +103,9 @@ class Model(object):
     def discriminate(self, segmap_image, fake_image, real_image, get_intermediate_features=False):
         """Returns the probability of the real_image and fake_image to be real."""
         #pred_real, pred_fake
-        return discriminator(segmap_image, real_image, self.opt.num_discriminators, self.opt.num_discriminator_filters, self.opt.num_discriminator_layers,
+        return discriminator(segmap_image, real_image, self.num_discriminators, self.num_discriminator_filters, self.num_discriminator_layers,
                              get_intermediate_features=get_intermediate_features), \
-               discriminator(segmap_image, fake_image, self.opt.num_discriminators, self.opt.num_discriminator_filters, self.opt.num_discriminator_layers,
+               discriminator(segmap_image, fake_image, self.num_discriminators, self.num_discriminator_filters, self.num_discriminator_layers,
                              get_intermediate_features=get_intermediate_features, reuse=tf.compat.v1.AUTO_REUSE)
 
 
@@ -88,13 +114,13 @@ class Model(object):
         z = None
         KLD_loss = None
 
-        if self.opt.use_vae:
-            mean, log_var = encoder(real_image, self.opt.crop_size, self.opt.num_encoder_filters)
+        if self.use_vae:
+            mean, log_var = encoder(real_image, self.crop_size, self.num_encoder_filters)
             z = tf.math.multiply(tf.random.normal(tf.shape(mean)), tf.math.exp(0.5 * log_var)) + mean
             if compute_kld_loss:
-                KLD_loss = kld_loss(mean, log_var) * self.opt.lambda_kld
+                KLD_loss = kld_loss(mean, log_var) * self.lambda_kld
 
-        return generator(segmap_image, self.opt.num_upsampling_layers, z, self.opt.z_dim, self.opt.num_generator_filters), \
+        return generator(segmap_image, self.num_upsampling_layers, z, self.z_dim, self.num_generator_filters), \
                KLD_loss
 
 # def use_gpu():
--- trainer/trainer.py
+++ trainer/trainer.py
@@ -16,20 +16,35 @@ class Trainer(object):
     """
     def __init__(self, sess, args):
         self.sess = sess
-        self.opt = args
+
+        # Save images
+        self.save_img_freq = args.save_img_freq
+        self.results_dir = args.results_dir
+
+        # Save model
+        self.save_model_freq = args.save_model_freq
+        self.checkpoint_dir = args.checkpoint_dir
+        self.checkpoint_filename = args.checkpoint_filename
+
+        # Training
+        self.epochs = args.epochs
+        self.batch_size = args.batch_size
+        self.batch_size = args.batch_size
 
         # Learning rate
         self.lr = tf.compat.v1.placeholder('float', name='learning_rate')
+        self.init_lr = args.lr
 
         # Initialize model
-        model = Model(sess, args)
+        model = Model(self.sess, args)
 
         # Load and shuffle data
         images, _, segmaps_onehot = load_data(args.image_dir, args.label_dir, args.semantic_label_path,
                                      img_size=(args.img_height,args.img_width), crop_size=args.crop_size,
                                      batch_size=args.batch_size, pairing_check=args.pairing_check)
-        self.n_batches = images.cardinality()//args.batch_size
-        photos_and_segmaps = tf.data.Dataset.zip((images, segmaps_onehot)).shuffle(self.n_batches, reshuffle_each_iteration=True)
+        n_batches = images.cardinality()//args.batch_size
+        self.iterations = n_batches.eval()
+        photos_and_segmaps = tf.data.Dataset.zip((images, segmaps_onehot)).shuffle(n_batches, reshuffle_each_iteration=True)
 
         # Define real image and associated one-hot segmentation map
         self.photos_and_segmaps_iterator = tf.compat.v1.data.make_initializable_iterator(photos_and_segmaps)
@@ -41,7 +56,8 @@ class Trainer(object):
         self.discriminator_loss = tf.math.reduce_sum(list(self.discriminator_losses.values()))
 
         # Construct optimizers
-        self.generator_optimizer, self.discriminator_optimizer = model.create_optimizers(self.generator_loss, self.discriminator_loss)
+        self.generator_optimizer, self.discriminator_optimizer = model.create_optimizers(self.generator_loss, self.discriminator_loss,
+                                                                                         self.lr, args.beta1, args.beta2, args.no_TTUR)
 
         # Define model saver
         self.saver = tf.compat.v1.train.Saver(max_to_keep=20)
@@ -51,29 +67,32 @@ class Trainer(object):
         tf.compat.v1.global_variables_initializer().run()
 
         # Initial learning rate
-        lr = self.opt.lr
+        lr = self.init_lr
 
         self.start_time = time.time()
 
-        if self.opt.batch_size == 1:
+        if self.batch_size == 1:
             save_imgs = self.save_img
-        for epoch in range(1, self.opt.epochs+1):
+        for epoch in range(1, self.epochs+1):
             self.sess.run(self.photos_and_segmaps_iterator.initializer)
-            for batch in range(1, self.n_batches.eval()+1):
+            for batch in range(1, self.iterations+1):
                 real_x, fake_x, _, g_loss, g_losses, _, d_loss, d_losses = self.sess.run([self.real, self.fake, self.generator_optimizer, self.generator_loss, self.generator_losses,
                                                       self.discriminator_optimizer, self.discriminator_loss, self.discriminator_losses],
                                                      feed_dict={self.lr: lr})
 
+                # self.print_info(g_loss, d_loss, epoch, batch)
                 self.print_info(g_loss, g_losses, d_loss, d_losses, epoch, batch)
 
                 # Save images
-                if np.mod(batch, self.opt.save_img_freq) == 0:
+                if np.mod(batch, self.save_img_freq) == 0:
                     # WARN('Saving images [epoch: %d, step: %d]' % (epoch, batch))
                     # save_imgs(real_x, 'real', epoch, batch)
-                    save_imgs(fake_x, 'fake', epoch, batch)
+                    # save_imgs(real_x, 'real_image', epoch, batch)
+                    # save_imgs(segmap_x, 'segmentation_map', epoch, batch)
+                    save_imgs(fake_x, 'synthesized_image', epoch, batch)
 
                 # Save model
-                if np.mod(batch, self.opt.save_model_freq) == 0:
+                if np.mod(batch, self.save_model_freq) == 0:
                     WARN('Saving the latest model [Epoch: %d, Step: %d]' % (epoch, batch))
                     self.save_model(batch)
 
@@ -82,7 +101,7 @@ class Trainer(object):
 
     def print_info(self, generator_loss, generator_losses, discriminator_loss, discriminator_losses, epoch, iteration):
         msg = "[Epoch: %3d/%3d, Iter: %3d/%3d, Time: %4.4f] - Generator loss: %.3f ~ " % (
-                    epoch, self.opt.epochs, iteration, self.n_batches.eval(), time.time() - self.start_time, generator_loss)
+                    epoch, self.epochs, iteration, self.iterations, time.time() - self.start_time, generator_loss)
 
         for key in generator_losses.keys():
             msg += "%s: %.3f, " % (key, generator_losses[key])
@@ -94,16 +113,16 @@ class Trainer(object):
         INFO(msg)
 
     def save_model(self, step):
-        if not os.path.exists(self.opt.checkpoint_dir):
-            os.makedirs(self.opt.checkpoint_dir)
+        if not os.path.exists(self.checkpoint_dir):
+            os.makedirs(self.checkpoint_dir)
 
-        self.saver.save(self.sess, os.path.join(self.opt.checkpoint_dir, self.opt.checkpoint_filename), global_step=step)
+        self.saver.save(self.sess, os.path.join(self.checkpoint_dir, self.checkpoint_filename), global_step=step)
 
     def save_img(self, img, type_img, epoch, step):
         img = tf.reshape(img, img.shape[1:])
-        if not os.path.exists(self.opt.results_dir):
-            os.makedirs(self.opt.results_dir)
+        if not os.path.exists(self.results_dir):
+            os.makedirs(self.results_dir)
 
         tf.keras.preprocessing.image.save_img(
-            '{}/{}_{:03d}_{:04d}.png'.format(self.opt.results_dir, type_img, epoch, step),
+            '{}/epoch{:03d}_iter{:04d}_{}.png'.format(self.results_dir, epoch, step, type_img),
             img.eval())
