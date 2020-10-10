6438b7e5449725db46cb9d15b3a1f274d42d3ea0
Mar Alguacil <maralguacil@correo.ugr.es>
2020-10-10 19:13:29 +0200

    Close #8 Config session

--- train.py
+++ train.py
@@ -1,7 +1,5 @@
 """
-    Builds and trains the Semantic Image Synthesis model, in addition to loading the data.
-
-    Author: Mar Alguacil
+    Train.
 """
 import tensorflow as tf
 from utils.pretty_print import *
@@ -10,14 +8,14 @@ from trainer.options import parse_args
 
 
 def main():
-    gpus= tf.config.experimental.list_physical_devices('GPU')
-    tf.config.experimental.set_memory_growth(gpus[0], True)
-
     # Parse arguments
     args = parse_args()
 
     # ~ with tf.device("/cpu:0"):
-    with tf.compat.v1.Session() as sess:
+    config = tf.compat.v1.ConfigProto()
+    config.gpu_options.allow_growth = True
+
+    with tf.compat.v1.Session(config=config) as sess:
         # Initialize trainer
         trainer = Trainer(sess, args)
 
